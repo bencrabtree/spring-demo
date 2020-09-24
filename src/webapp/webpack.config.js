d9b82dceb946f55d2716186c7e88fdfa9ebbc429
bencrab <bencrab@umich.edu>
2020-09-24 16:04:53 -0400

    Webpack integration

---
+++ src/webapp/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    entry: [ 'babel-polyfill', './src/index.js' ],
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.jsx', '.js', '.scss', '.css']
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'public/js')
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
+}
